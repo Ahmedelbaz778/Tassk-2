1080292b21f7cb1f1bebe714dcb695f4e58bd799
Ahmedelbaz778 <115692852+Ahmedelbaz778@users.noreply.github.com>
2022-12-23 17:26:16 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number to start counting from: ");
+            int num = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the number to stop counting to: ");
+            int numf = int.Parse(Console.ReadLine());
+
+            for (int c = num; c <= numf; c++)
+            {
+
+                int sum = 0;
+                for (int x = 1; x < c; x++)
+                {
+                    if (c % x == 0)
+                        sum += x;
+                    else
+                        continue;
+                }
+                if (sum == c)
+                {
+                    Console.WriteLine(c);
+                }
+
+            }
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
